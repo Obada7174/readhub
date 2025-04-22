a492997a83e3f31fb17e7b173920d83f066c237a
Obada7174 <obada7174@gmail.com>
2025-04-22 02:01:40 -0700

    i18n commit

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,20 @@
-import type { NextConfig } from "next";
-const { i18n } = require('./next-i18next.config');
+// import type { NextConfig } from "next";
+// // import nextConfigI18n from './next-i18next.config';
+// const nextConfig: NextConfig = {
+//   experimental: {
+//     serverActions: {},
+//   },
+//   // i18n: {
+//   //   locales: ['en', 'ar'],
+//   //   defaultLocale: 'en'
+//   // }
+// };
 
-const nextConfig: NextConfig = {
-  i18n,
-  /* config options here */
-};
-
-export default nextConfig;
+// export default nextConfig;
+import {NextConfig} from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
+ 
+const nextConfig: NextConfig = {};
+ 
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(nextConfig);
