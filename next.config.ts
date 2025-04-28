82e82919cbcbd3e2dfaeba97fb86a6b41e94c2b8
Obada7174 <obada7174@gmail.com>
2025-04-28 04:17:05 -0700

    modified:   next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,3 @@
-// import type { NextConfig } from "next";
-// // import nextConfigI18n from './next-i18next.config';
-// const nextConfig: NextConfig = {
-//   experimental: {
-//     serverActions: {},
-//   },
-//   // i18n: {
-//   //   locales: ['en', 'ar'],
-//   //   defaultLocale: 'en'
-//   // }
-// };
-
-// export default nextConfig;
 import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
