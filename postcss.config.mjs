07ad32e46100db5523340c3fec4bc27df4702b4a
Obada7174 <obada7174@gmail.com>
2025-04-28 04:44:16 -0700

    Reset project with new content

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,14 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  theme:{
+    extend:{
+      colors:{
+        "beige-100":"",
+        "beige-200":"",
+        "beige-300":"",
+      }
+    }
+  }
 };
 
 export default config;
