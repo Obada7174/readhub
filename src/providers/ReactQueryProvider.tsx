10abc75105d218fd5c4702148eb423981df58c45
Obada7174 <obada7174@gmail.com>
2025-05-16 07:20:53 -0700

    fixes

--- src/providers/ReactQueryProvider.tsx
+++ src/providers/ReactQueryProvider.tsx
@@ -1,11 +1,28 @@
-'use client';
+"use client";
 
-import React from 'react';
-import { QueryClientProvider } from '@tanstack/react-query';
-import { getQueryClient } from '@/lib/queryClient';
+import { ReactNode } from "react";
+import {
+    QueryClientProvider,
+    HydrationBoundary,
+} from "@tanstack/react-query";
+import { getQueryClient } from "@/lib/queryClient";
 
-const queryClient = getQueryClient();
+type Props = {
+    children: ReactNode;
+    dehydratedState: unknown;
+};
 
-export default function ReactQueryProvider({ children }: React.PropsWithChildren) {
-    return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+export default function ReactQueryProvider({
+    children,
+    dehydratedState,
+}: Props) {
+    const queryClient = getQueryClient();
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <HydrationBoundary state={dehydratedState ?? {}}>
+                {children}
+            </HydrationBoundary>
+        </QueryClientProvider>
+    );
 }
